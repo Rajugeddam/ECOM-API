613bb1e4b018f43a78321906499121cf7d44888d
Rajugeddam <102058534+Rajugeddam@users.noreply.github.com>
2023-12-09 12:43:05 +0530

    Add files via upload

---
+++ controllers/api/v1/doctor_controller.js
@@ -0,0 +1,47 @@
+const Doctor = require('../../../models/doctor');
+const jwt = require('jsonwebtoken');
+
+// register the doctor
+module.exports.registerDoctor = async function(req, res){
+    try {
+        let createDoctor = await Doctor.findOne({email: req.body.email});
+        if(!createDoctor){
+            await Doctor.create(req.body)
+        }
+        res.status(200).json({
+            success: true,
+            body: createDoctor,
+            message: "Doctor register succesfully"
+        })
+    } catch (error) {
+        console.log("Error", error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server Error"
+        })
+    }
+}
+
+// login the doctor
+module.exports.createSession = async function(req, res){
+    try {
+        let findDoctor = await Doctor.findOne({email: req.body.email});
+        if(!findDoctor || findDoctor.password !== req.body.password){
+            return res.status(422).json({
+                message: "Invalid user name or password"
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: "sign in successfully, here is the token",
+            data:{
+                token: jwt.sign(findDoctor.toJSON(), 'hospital')
+            }
+        })
+    } catch (error) {
+        console.log("Error", error);
+        return res.status(500).json({
+            message: "Internal server error",
+        })
+    }
+}
